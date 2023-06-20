aff6391e7f2043d0b95ca3022280cba4b00e824c
zhangjin86 <zhangjin86@midea.com>
2023-06-20 19:04:20 +0800

    add mainpage text blocks.

--- note2/src/main/java/com/test/note2/MainActivity.kt
+++ note2/src/main/java/com/test/note2/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,7 +13,14 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -34,6 +43,7 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.test.note2.ui.theme.MyApplicationTheme
+import com.test.note2.ui.theme.MyTypography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,20 +62,110 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+abstract class Note(open var title: String, open var date: String) {
+    @Composable
+    abstract fun Item();
+}
+
+class TextNode constructor(
+    var text: String,
+    var color: Color,
+    override var title: String,
+    override var date: String
+) :
+    Note(title, date) {
+
+    init {
+        this.text = text;
+        this.date = date;
+        this.title = title;
+    }
+
+    @Composable
+    override fun Item() {
+        Column(
+            modifier = Modifier
+                .background(color)
+                .fillMaxWidth()
+                .sizeIn(maxHeight = 130.dp)
+                .padding(10.dp)
+                .verticalScroll(enabled = true, state = ScrollState(1))
+        ) {
+            Text(text = title, style = MyTypography.titleMedium)
+            Text(
+                modifier = Modifier.sizeIn(maxHeight = 60.dp),
+                text = text,
+                style = MyTypography.bodyMedium
+            )
+            Text(text = date, style = MyTypography.bodySmall)
+        }
+    }
+}
+
 @Composable
 fun MainPage() {
+    val t1: TextNode = TextNode("标题1", Color.Blue, "正文", "2023-06-20")
+    val t2: TextNode = TextNode(
+        title = "标题2",
+        color = Color.Green,
+        text = "正sdflkajsdsjkldflskfdjksdlflkjdddddddddddddd ddddddddddddddddddddddd\ndddddddddddddddddddddddddddddddddddddddddd222222",
+        date = "2023-06-20"
+    )
+    val list = listOf<Note>(t1, t2)
+    Row() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top
+        ) {
+            for (t in list) {
+                if (t is TextNode) {
+                    val tt: TextNode = t
+                    Row(
+                        Modifier
+                            .padding(5.dp, 5.dp, 5.dp, 0.dp)
+                            .fillMaxWidth()
+                    ) {
+                        tt.Item()
+                    }
+                }
+            }
+        }
+    }
+
     var navSel by remember {
         mutableStateOf(1)
     }
-    Text(text = "测试")
+
     Row() {
         Column(
-            modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Bottom
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            EditButton()
             BottomNav(sel = navSel, onClick1 = { navSel = 1 }, onClick2 = { navSel = 2 })
         }
     }
 
+}
+
+@Composable
+fun EditButton(width: Dp = 80.dp) {
+    Button(modifier = Modifier
+        .offset(0.dp, width / 2)
+        .height(width)
+        .width(width),
+        shape = RoundedCornerShape(width / 2, width / 2, width / 2, width / 2),
+        colors = ButtonDefaults.buttonColors(containerColor = Color(160, 198, 157)),
+        onClick = { /*TODO*/ }) {
+        Image(
+            modifier = Modifier
+                .height(40.dp)
+                .width(40.dp),
+            painter = painterResource(id = R.drawable.edit),
+            contentDescription = "edit"
+        )
+    }
 }
 
 @Composable
@@ -94,8 +194,22 @@ fun BottomNav(
             .height(navHeight),
         horizontalArrangement = Arrangement.SpaceAround
     ) {
-        NavItem(contentId = R.string.nav1_label,imageHeight=imageHeight, fontSize = fontSize, onClick = onClick1, nav_resource_id = nav1_resource_id, fontWeight = fontWeight1)
-        NavItem(contentId = R.string.nav2_label,imageHeight=imageHeight, fontSize = fontSize, onClick = onClick2, nav_resource_id = nav2_resource_id, fontWeight = fontWeight2)
+        NavItem(
+            contentId = R.string.nav1_label,
+            imageHeight = imageHeight,
+            fontSize = fontSize,
+            onClick = onClick1,
+            nav_resource_id = nav1_resource_id,
+            fontWeight = fontWeight1
+        )
+        NavItem(
+            contentId = R.string.nav2_label,
+            imageHeight = imageHeight,
+            fontSize = fontSize,
+            onClick = onClick2,
+            nav_resource_id = nav2_resource_id,
+            fontWeight = fontWeight2
+        )
     }
 }
 
@@ -113,13 +227,22 @@ fun NavItem(
         colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
         onClick = onClick
     ) {
-        Column(modifier = Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Column(
+            modifier = Modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Image(
                 modifier = Modifier.size(imageHeight),
                 painter = painterResource(id = nav_resource_id),
                 contentDescription = ""
             )
-            Text(stringResource(id = contentId), fontSize = fontSize, color = color, fontWeight = fontWeight)
+            Text(
+                stringResource(id = contentId),
+                fontSize = fontSize,
+                color = color,
+                fontWeight = fontWeight
+            )
         }
     }
 }
